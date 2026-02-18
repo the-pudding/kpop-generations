3ebece38b4973ffb4a6985902369b5b5066a0324
Michelle McGhee <michmac202@gmail.com>
2026-02-18 15:34:35 -0800

    concerts chart closes #18

--- src/components/Section.Chart.svelte
+++ src/components/Section.Chart.svelte
@@ -2,6 +2,7 @@
 	import DebutChart from "$components/Section.DebutChart.svelte";
 	import GangnamChart from "$components/Section.GangnamChart.svelte";
 	import YouTubeChart from "$components/Section.YouTubeChart.svelte";
+	import ConcertsChart from "$components/Section.ConcertsChart.svelte";
 	import themes from "$data/themes.json";
 	import { getContext, onMount } from "svelte";
 	import _ from "lodash";
@@ -23,7 +24,8 @@
 	const components = {
 		debuts: DebutChart,
 		"gangnam-style": GangnamChart,
-		"youtube-records": YouTubeChart
+		"youtube-records": YouTubeChart,
+		concerts: ConcertsChart
 	};
 	const C = components[id];
 </script>
---
+++ src/components/Section.ConcertsChart.svelte
@@ -0,0 +1,16 @@
+<script>
+	import concertSvg from "$svg/concerts-chart.svg";
+
+	let { title } = $props();
+</script>
+
+<h3>{title}</h3>
+{@html concertSvg}
+
+<style>
+	h3 {
+		text-transform: uppercase;
+		text-align: center;
+		font-weight: bold;
+	}
+</style>
