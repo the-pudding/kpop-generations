796b19d08de5202d48801d1f287e1c4d6af6dab8
Jan Diehm <jadiehm@gmail.com>
2026-03-16 14:46:06 -0500

    update github pages

--- docs/_app/immutable/entry/start.CWyKnmai.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/jS5O7hhU.js";export{o as load_css,r as start};
---
+++ docs/_app/immutable/entry/start.Dw79rHLz.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/CStp11ED.js";export{o as load_css,r as start};
